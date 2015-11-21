c950b00bfab34a7f2e27e99d64c97c1afc2f907d
carlesm95 <carlesm.95@gmail.com>
2015-11-21 12:32:21 +0100

    Main

--- Programación/2-Database constructor/Programa_principal.py
+++
@@ -1,8 +0,0 @@
-import os
-from build_database import build_database
-ruta=os.path.dirname(os.path.abspath(__file__))
-path1 = ruta + '\\TerrassaBuildings900\\train\\images'
-path2 = ruta + '\\TerrassaBuildings900\\valid\\images'
-savepath = ruta + '\\Build_database'
-build_database(path1, savepath)
-build_database(path2, savepath)
