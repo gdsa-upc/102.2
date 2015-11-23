5dfbdda711da3fcb55d1c86d8778ad11de9fe017
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-11-23 11:39:56 +0100

    Update get_features.py

--- Programación/3- Image descriptor/get_features.py
+++ Programación/3- Image descriptor/get_features.py
@@ -11,13 +11,17 @@ def get_features(diropen,dirIDs,dirsave):
     feat = open (dirsave+ '\\Features.txt', 'w')  
     #Declarem el dictionary
     dic1=dict()
+    #Creació d'una llista buida
     lrand=[]
     for line in ID:
+        #Creació d'un vector de mida 100 amb valors aleatoris més petits que 1
         lrand=np.random.rand(1,100)
         dic1[line.replace('\n','')]=lrand;
+    #Tanquem el .txt d'entrada
     ID.close();
-    #Exportem el dictionary a un fitxer de text
+    #Exportem el dictionari a un fitxer de text
     pk.dump(dic1,feat);
+    #Tanquem el .txt de sortida
     feat.close();
 
 
