61c2d906e0f148fc56af5064e8576344f9794467
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-11-27 22:26:12 +0100

    Update get_assigments.py

--- Programación/3- Image descriptor/get_assigments.py
+++ Programación/3- Image descriptor/get_assigments.py
@@ -1,6 +1,8 @@
 from scipy.cluster.vq import vq
 from sklearn import preprocessing
+
 def get_assignments (descriptores,codebook):
+    #Normalitzem els valors
     des=preprocessing.normalize(descriptores)
     #Calculem les assignacions i omplim els parametres (code i dist)
     [code,dist]=vq(des,codebook)
