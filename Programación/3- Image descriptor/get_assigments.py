52655d8ad74e755d8813472179a35485849a5747
Carles Morales <carlesm.95@gmail.com>
2015-11-27 21:07:01 +0100

    Update get_assigments.py

--- Programación/3- Image descriptor/get_assigments.py
+++ Programación/3- Image descriptor/get_assigments.py
@@ -1,19 +1,9 @@
-import cv2
-import numpy as np
-from numpy import array
-import os 
-import sklearn.metrics
-import scipy
-from get_params import get_params
-from scipy.cluster.vq import vq, kmeans, whiten
-
-#Importem la funció train_codebook
-from train_codebook import train_codebook
-
-
+from scipy.cluster.vq import vq
+from sklearn import preprocessing
 def get_assignments (descriptores,codebook):
+    des=preprocessing.normalize(descriptores)
     #Calculem les assignacions i omplim els parametres (code i dist)
-    [code,dist]=vq(descriptores,codebook)
+    [code,dist]=vq(des,codebook)
     
     #Retornem el vector amb les assignacions per cada descriptor
     return assignments
