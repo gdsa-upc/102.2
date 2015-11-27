eeb5fef5b6983482e98bade5cafdc7aaaa7deda8
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-11-27 19:15:31 +0100

    Update get_local_features.py

--- Programación/3- Image descriptor/get_local_features.py
+++ Programación/3- Image descriptor/get_local_features.py
@@ -9,23 +9,19 @@ from get_params import get_params
 def get_local_features(params):
     #Obrim .txt d'entrada
     ID =open(os.path.join(params['root'],params['database'],'val','ImageIDs.txt'), 'r')
-    #Obrim .txt de sortida
-    feat = open('C:\Users\Gerard\Documents\Universidad\TercerCurso\GDSA\Proyecto\Codigo\pruebalocalfeat.txt')  
     #Declarem el dictionary
-    #dic1=dict()
+    dic1=dict()
     orb=cv2.ORB()
-    #ORB obtiene 500 features de serie 
-        #Recorrem cada Imatge 
+    #ORB obté 500 features
+        #Bucle per recorre cada imatge
     for line in ID:
-        #Ahora tenemos que calcular ORB para cada imagen y guardarlo!
-        ##leemos
+        #Calcular ORB per a casa imatge
         img =  cv2.imread(os.path.join(params['root'],params['database'],'val','images',str(line).replace('\n','') + '.jpg'))
         kp=orb.detect(img,None)
-        kp, des= orb.compute(img,kp,params['descriptor_size']) #computamos
-        #guardamos para cada valor de la imagen analizada sus descriptores
-        ##Debemos guardar todos los descriptores en una matriz
-        #dic1[line.replace('\n','')]=des
-    return des
+        kp, des= orb.compute(img,kp,params['descriptor_size']) 
+        #Guardem els valors al diccionari
+        dic1[line.replace('\n','')]=des
+    return dic1
     ID.close()
 
 
