95349c24d3f0e96abdba965db7a6ce16916d844a
Carles Morales <carlesm.95@gmail.com>
2015-11-27 21:18:46 +0100

    Update Main_Sessio5.py

--- Programación/Main_Sessio5.py
+++ Programación/Main_Sessio5.py
@@ -10,3 +10,12 @@ for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(line).replace('\n','') + '.jpg'))
     desc=np.concatenate((desc,x))
 ID.close()
+
+codebook=train_codebook(params,desc)
+
+ID =open(os.path.join(params['root'],params['database'],'val','ImageIDs.txt'), 'r')
+desc_val=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(ID.readline()).replace('\n','') + '.jpg'))
+for line in ID:
+    x=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(line).replace('\n','') + '.jpg'))
+    desc_val=np.concatenate((desc,x))
+ID.close()
