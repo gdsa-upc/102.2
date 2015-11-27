e426758427c726c8d037a59de14745f1bd070210
Carles Morales <carlesm.95@gmail.com>
2015-11-27 17:47:19 +0100

    Update train_codebook.py

--- Programación/3- Image descriptor/train_codebook.py
+++ Programación/3- Image descriptor/train_codebook.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 ##train codebook
 
-import cv2
-import numpy as np
-from numpy import array
-import os 
-import sklearn.metrics
+#import cv2
+#import numpy as np
+#from numpy import array
+#import os 
+#import sklearn.metrics
 import scipy
-from get_params import get_params
-from scipy.cluster.vq import vq, kmeans, whiten
+from scipy.cluster.vq import kmeans, whiten
+#from get_params import get_params
 
 from get_local_features import get_local_features
 
@@ -16,10 +16,10 @@ def train_codebook(descriptores):
     des=whiten(descriptores)
     #Entrenamos el codebook, 4 es el numero de cluster que queremos 
     codebook={}
-    codebook = scipy.cluster.vq.kmeans(des, 4, iter=20, thresh=1e-05, check_finite=True)
+    codebook = kmeans(des, k_or_guess=4.0)
     return codebook
     
     
 #Para probarlo
-params=get_params()
-train_codebook(get_local_features(params))
+#params=get_params()
+#train_codebook(get_local_features(params))
