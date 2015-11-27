61143f20898752c67440da7babc286d9be8863ba
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-11-27 18:51:54 +0100

    Update get_assigments.py

--- Programación/3- Image descriptor/get_assigments.py
+++ Programación/3- Image descriptor/get_assigments.py
@@ -7,7 +7,9 @@ import scipy
 from get_params import get_params
 from scipy.cluster.vq import vq, kmeans, whiten
 
+#Importem la funció train_codebook
 from train_codebook import train_codebook
+
 
 def get_assignments (descriptores,codebook):
     #Calculem les assignacions
