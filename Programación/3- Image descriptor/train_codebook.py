eda318424efaf27ffbce039607e1272cc83c68b5
Carles Morales <carlesm.95@gmail.com>
2015-11-28 17:48:17 +0100

    Update train_codebook.py

--- Programación/3- Image descriptor/train_codebook.py
+++ Programación/3- Image descriptor/train_codebook.py
@@ -3,10 +3,9 @@
 from scipy.cluster.vq import kmeans
 from sklearn import preprocessing
 
-def train_codebook(params,descriptores):
+def train_codebook(params,descriptores,paraules):
     #Normalitzem els valors dels descriptors
     des=preprocessing.normalize(descriptores)
     #Entrenem el KMeans, ajustem el número de clusters a 4
-    [codebook,dist] = kmeans(des, k_or_guess=4.0)
-   
+    [codebook,dist] = kmeans(des, k_or_guess=paraules)
     return codebook
