5873fc614dac392cebebc344aea78b9759dcb613
Carles Morales <carlesm.95@gmail.com>
2015-11-28 12:05:01 +0100

    Update build_bow.py

--- Programación/3- Image descriptor/build_bow.py
+++ Programación/3- Image descriptor/build_bow.py
@@ -3,9 +3,10 @@ import numpy as np
 import cPickle as pk
 from sklearn import preprocessing
 
-def build_bow(assignments,codebook, id_images):
+def build_bow(assignments,codebook):
     #Inicialitzem la llista bag of words amb tants zeros com assignments diferents hi hagi
     bow=[0]*len(set(assignments))
     for i in range(0,len(assignments)):
         bow[i]+=1
     bow=preprocessing.normalize(bow)
+    return bow
