55e866718d26117ffb48b899bc5b58c8b1e9a7b8
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-11-28 13:10:23 +0100

    Update Main_Sessio5.py

--- Programación/Main_Sessio5.py
+++ Programación/Main_Sessio5.py
@@ -11,6 +11,7 @@ from build_bow import build_bow
 params=get_params()
 ID =open(os.path.join(params['root'],params['database'],'train','ImageIDs.txt'), 'r')
 desc_train=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(ID.readline()).replace('\n','') + '.jpg'))
+
 #Extraccio de les características per a totes les imatges d'entrenament
 for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(line).replace('\n','') + '.jpg'))
@@ -19,28 +20,40 @@ for line in ID:
 
 #Entrenament del KMeans només per les fotos d'entrenament
 codebook=train_codebook(params,desc_train)
+#Calculem les assignacions per les imatges d'entrenament
 assignments=get_assignments(desc_train,codebook)
-
+#Creació del diccionari
 dictrain=dict()
 dictrain[str(ID.readline()).replace('\n','')]=build_bow(assignments,codebook)
+
 for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(line).replace('\n','') + '.jpg'))
+    #Calculem les assignacions per les imatges d'entrenament
     assignments=get_assignments(x,codebook)
+    #Creació del BoW per les imatges d'entrenament i emplenament del diccionari
     dictrain[str(line).replace('\n','')]=build_bow(assignments,codebook)
 ID.close()
 
+#Creació del BoW per les imatges d'entrenament
 bow_train = open (os.path.join(params['root'],params['database'],'train','bow_train.txt'), 'w')
 pk.dump(dictrain,bow_train)
 bow_train.close()
 
+#Extraccio de les características per a totes les imatges de validacio
 ID = open(os.path.join(params['root'],params['database'],'val','ImageIDs.txt'), 'r')
 desc_val=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(ID.readline()).replace('\n','') + '.jpg'))
+
+#Calculem les assignacions per les imatges de validacio
 assignments=get_assignments(desc_val,codebook)
+#Creació del diccionari
 dicval=dict()
 dicval[str(ID.readline()).replace('\n','')]=build_bow(assignments,codebook)
+
 for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(line).replace('\n','') + '.jpg'))
+    #Calculem les assignacions per les imatges de validacio
     assignments=get_assignments(x,codebook)
+    #Creació del BoW per les imatges de validacio i emplenament del diccionari
     dicval[str(line).replace('\n','')]=build_bow(assignments,codebook)
 ID.close()
 
