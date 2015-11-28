932a95be2fd377c44697f2d1b1ebe6e83254767d
Ivancaminal72 <ivancaminal72@gmail.com>
2015-11-28 15:16:15 +0100

    Ranking [Versió Final]

--- Programación/4- Random ranking/rank.py
+++ Programación/4- Random ranking/rank.py
@@ -1,28 +1,50 @@
 # -*- coding: utf-8 -*-
-## Random_ranking
+## Ranking
 import os
 import random
 import numpy as np
 import cPickle as pk
+import sklearn as sklearn
+from get_params import get_params
 
-def rank(pathval,pathtrain,outpath):
+
+def rank(params):
     #Obrim els fitxers de característiques de validació i train
-    fval= open(pathval+'\\Features.txt','r')
-    ftrain= open(pathtrain+'\\Features.txt','r')
+    fval= open((os.path.join(params['root'],params['database'],'val','Features.txt')),'r')
+    ftrain= open((os.path.join(params['root'],params['database'],'train','Features.txt')),'r')
     #Declarem els dictionaries
     dval=dict()
     dtrain=dict()
     #Carreguem la informació als dictionaries
     dval=pk.load(fval)
     dtrain=pk.load(ftrain)
-    #Per cada imatge de Validació generem un ranking aleatori
+    #Generem una llista amb les keys de train
+    lktrain=dtrain.keys()
+    #Generem una llista amb els histogrames del diccionari train
+    lhtrain=list()
+    for tkey in lktrain:
+        lhtrain.append(dtrain[tkey])
+        
+    #Declaració de variables 
+    ldist=list()#Llista de distàncies entre l'histograma de validació i els de train
+    tup=()#Tupla on guardarem la ID de train i la distància al histograma de validació 
+    ltup=list()#Llista de tuples 
+    #Per cada imatge de Validació generem un ranking 
     for vkey in dval:
-        frank = open(outpath+'\\'+vkey+'.txt','w')
-        for tkey in dtrain:
-            frank.write(random.choice(dtrain.keys())+"\n")
+        #Obrim el fitxer on escriurem el ranking per la imatge de validació
+        frank = open((os.path.join(params['root'],params['database'],'val','result',vkey,'.txt')),'w')
+        #Calculem la distància entre l'histograma de validació i els de train
+        ldist=sklearn.metrics.pairwise.pairwise_distances(dval[vkey],lhtrain)
+        #Creem una llista de tuples amb les IDs i les distancies de train
+        for tkey in lktrain:
+            tup=(tkey,ldist.pop[0])
+            ltup.append(tup)
+        #Ordenem la llista segons la distancia 
+        sorted(ltup, key=lambda distancia: distancia[1])
+        #Escrivim cada ID ordenada al fitxer rank
+        for tupla in ltup:
+            frank.write(tupla[0]+"\n")
         frank.close()
     #Tanquem els fitxers de característiques de validació i train
     fval.close()
     ftrain.close()
-    
-    
