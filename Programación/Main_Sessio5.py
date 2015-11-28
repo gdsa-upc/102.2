992f617295f1d20f0bdc3ae86e86002e8662c631
Carles Morales <carlesm.95@gmail.com>
2015-11-28 12:46:51 +0100

    Update Main_Sessio5.py

--- Programación/Main_Sessio5.py
+++ Programación/Main_Sessio5.py
@@ -10,25 +10,41 @@ from build_bow import build_bow
 #Extraccio dels parametres
 params=get_params()
 ID =open(os.path.join(params['root'],params['database'],'train','ImageIDs.txt'), 'r')
-desc=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(ID.readline()).replace('\n','') + '.jpg'))
+desc_train=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(ID.readline()).replace('\n','') + '.jpg'))
 #Extraccio de les características per a totes les imatges d'entrenament
 for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(line).replace('\n','') + '.jpg'))
     #Concatenar les caracteristiques de cada imatge
-    desc=np.concatenate((desc,x))
-ID.close()
+    desc_train=np.concatenate((desc_train,x))
 
 #Entrenament del KMeans només per les fotos d'entrenament
-codebook=train_codebook(params,desc)
+codebook=train_codebook(params,desc_train)
+assignments=get_assignments(desc_train,codebook)
 
-ID =open(os.path.join(params['root'],params['database'],'val','ImageIDs.txt'), 'r')
+dictrain=dict()
+dictrain[str(ID.readline()).replace('\n','')]=build_bow(assignments,codebook)
+for line in ID:
+    x=get_local_features(params,os.path.join(params['root'],params['database'],'train','images',str(line).replace('\n','') + '.jpg'))
+    assignments=get_assignments(x,codebook)
+    dictrain[str(line).replace('\n','')]=build_bow(assignments,codebook)
+ID.close()
+
+bow_train = open (os.path.join(params['root'],params['database'],'train','bow_train.txt'), 'w')
+pk.dump(dictrain,bow_train)
+bow_train.close()
+
+ID = open(os.path.join(params['root'],params['database'],'val','ImageIDs.txt'), 'r')
 desc_val=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(ID.readline()).replace('\n','') + '.jpg'))
 assignments=get_assignments(desc_val,codebook)
-dic1=dict()
-dic1[str(ID.readline()).replace('\n','')]=build_bow(assignments,codebook)
+dicval=dict()
+dicval[str(ID.readline()).replace('\n','')]=build_bow(assignments,codebook)
 for line in ID:
     x=get_local_features(params,os.path.join(params['root'],params['database'],'val','images',str(line).replace('\n','') + '.jpg'))
     assignments=get_assignments(x,codebook)
-    dic1[str(line).replace('\n','')]=build_bow(assignments,codebook)
+    dicval[str(line).replace('\n','')]=build_bow(assignments,codebook)
 ID.close()
+
+bow_val = open (os.path.join(params['root'],params['database'],'val','bow_val.txt'), 'w')
+pk.dump(dicval,bow_val)
+bow_val.close()
 
