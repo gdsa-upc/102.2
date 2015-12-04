3a0aa99d1b95c6ff861963287bdb01756f653715
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-12-04 11:26:01 +0100

    Update train_classifier.py

--- Programación/5- Random classification/train_classifier.py
+++ Programación/5- Random classification/train_classifier.py
@@ -7,6 +7,8 @@ def train_classifier(params,caracteristiques_train):
     annotation = open(os.path.join(params['root'],params['database'],'train','annotation.txt'),'r')
     labels=[]
     for line in annotation:
+        #Afegim a la variable label totes les classes tretes del .txt de les annotacions
         labels.append(line.split()[0])
+    #Eliminem classes repetides amb la funcio set
     labels=set(labels)
     clf.fit(caracteristiques_train,labels)
