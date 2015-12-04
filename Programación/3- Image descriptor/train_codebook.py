2ba1e344902902fc12a58575cfc4e5114c6beb92
Carles Morales <carlesm.95@gmail.com>
2015-12-04 13:36:41 +0100

    Update train_codebook.py

--- Programación/3- Image descriptor/train_codebook.py
+++ Programación/3- Image descriptor/train_codebook.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-
-from scipy.cluster.vq import kmeans
-from sklearn import preprocessing
+from sklearn.cluster import MiniBatchKMeans
 
 def train_codebook(params,descriptores,paraules):
-    #Normalitzem els valors dels descriptors amb la normalització L2
-    des=preprocessing.normalize(descriptores)[0]
+    km = MiniBatchKMeans(paraules)
     #Entrenem el KMeans, ajustem el número de clusters a 4
-    [codebook,dist] = kmeans(des, k_or_guess=paraules)
-    return codebook
+    km.fit(descriptores)
+    return km
