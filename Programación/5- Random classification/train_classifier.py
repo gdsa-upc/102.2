1e352c2198cc631d2960a2970d2b9a28c4db641c
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-12-04 16:39:31 +0100

    Update train_classifier.py

--- Programación/5- Random classification/train_classifier.py
+++ Programación/5- Random classification/train_classifier.py
@@ -2,14 +2,17 @@ import os
 from sklearn import svm
 
 def train_classifier(params,caracteristiques_train):
-    clf=svm.SVC()
+    clf = svm.SVC()
     #Obrim el fitxer de les anotacions
     annotation = open(os.path.join(params['root'],params['database'],'train','annotation.txt'),'r')
     #Creacio de la llista buida
-    labels=[]
+    labels = []
+    #Bucle per obtenir les 'labels' del annotation.txt
     for line in annotation:
         #Afegim a la variable label totes les classes tretes del .txt de les annotacions
+        #utilitzant la funcio split( ) i sppend( ) 
         labels.append(line.split()[0])
     #Eliminem classes repetides amb la funcio set
-    labels=set(labels)
+    labels = set(labels)
+    #Clasificacio entre les 'features' i les classes 'labels'
     clf.fit(caracteristiques_train,labels)
