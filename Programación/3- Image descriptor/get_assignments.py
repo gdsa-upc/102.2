70bc7924f3c43486c57c659e9f5020acf649efbd
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-12-04 16:14:39 +0100

    Update get_assignments.py

--- Programación/3- Image descriptor/get_assignments.py
+++ Programación/3- Image descriptor/get_assignments.py
@@ -1,6 +1,6 @@
 def get_assignments (descriptores,codebook):
-    #Calculem les assignacions
-    assignments=codebook.predict(descriptores)
     
+    #Calculem les assignacions mitjançant la funció predict( )
+    assignments=codebook.predict(descriptores)
     #Retornem el vector amb les assignacions per cada descriptor
     return assignments
