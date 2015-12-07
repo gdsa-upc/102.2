b6847504876e8e68c9401d687a97da3d081caa12
Ivan Caminal <ivancaminal72@gmail.com>
2015-12-07 14:09:31 +0100

    Actualització train_classifier 07/12/2015

--- Programación/5- Random classification/train_classifier.py
+++ Programación/5- Random classification/train_classifier.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from sklearn import svm
 import cPickle as pk
@@ -15,19 +16,47 @@ def train_classifier(params):
         #Afegim a la variable label totes les classes tretes del .txt de les annotacions
         #utilitzant la funcio split( ) i append( ) 
         labels.append(line.split()[1])
+    annotation.close()
     #Declarem el diccionari amb les labels i el pes que tindran
     dic_labels=dict()
     #Omplim el diccionari amb les labels i els seus pesos
     for label in set(labels):
-        if label is not 'desconegut':
-            dic_labels[label]=1
+        if label == 'desconegut':
+            dic_labels[label]=0.17
         else:
-           dic_labels[label]=0.17 
+           dic_labels[label]=1
+    #Declaracio de la clf
     clf = svm.SVC(gamma=0.001, C=100, class_weight=dic_labels)
+    #Obrim el fitxer Features.txt de train
     bow_train= open((os.path.join(params['root'],params['database'],'train','Features.txt')),'r')
-    caracteristiques_train=dict()
-    caracteristiques_train=pk.load(bow_train)
-    diclist=[]
-    for key,value in caracteristiques_train.iteritems():
-        diclist.append(value)
-    print clf.fit(diclist,labels)
+    #Creem i omplim el diccionari de train
+    dftrain=dict()
+    dftrain=pk.load(bow_train)
+    #Reobrim el fitxer de les anotacions
+    annotation = open(os.path.join(params['root'],params['database'],'train','annotation.txt'),'r')
+    #Saltem la primera linia del fitxer
+    annotation.readline()
+    #Creem un diccionari amb les ImageIDs de train i el label
+    dklab=dict()
+    for line in annotation:
+        #Afegim a la variable label totes les classes tretes del .txt de les annotacions
+        #utilitzant la funcio split( ) i append( ) 
+        dklab[line.split()[0]]=line.split()[1]
+    annotation.close()
+    
+    #Creem dues llistes on hi hagin les "features" i els "labels" ORDENATS PER IMATGE!!
+    lf=list()
+    ll=list()
+    for key in dklab.keys():
+        ll.append(dklab[key])
+        lf.append(dftrain[key])
+        
+    lf=np.array(lf)
+    ll=np.array(ll)
+    #Entrenem el model SVM amb les dades d'entrenament
+    clf.fit(lf,ll) # Mirar ejemplo de Model Persistence: http://scikit-learn.org/stable/tutorial/basic/tutorial.html
+    
+    #Guardem el model a memoria. El pk.dumps guarda el model en un string en comptes de en un fitxer
+    trained_model = open (os.path.join(params['root'],params['database'],'train','Trained_model.txt'), 'w')
+    pk.dump(clf,trained_model)
+    trained_model.close()
