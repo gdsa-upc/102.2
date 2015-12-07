260ab9d0db2eed1d8ace4749d85d7877cca3d9a4
Ivancaminal72 <ivancaminal72@gmail.com>
2015-12-07 14:01:27 +0100

    Train_classifier 07/12/2015 14:01

--- Programación/5- Random classification/train_classifier.py
+++ Programación/5- Random classification/train_classifier.py
@@ -21,28 +21,22 @@ def train_classifier(params):
     dic_labels=dict()
     #Omplim el diccionari amb les labels i els seus pesos
     for label in set(labels):
-        if label is not 'desconegut':
-            dic_labels[label]=1
+        if label == 'desconegut':
+            dic_labels[label]=0.17
         else:
-           dic_labels[label]=0.17
+           dic_labels[label]=1
     #Declaracio de la clf
     clf = svm.SVC(gamma=0.001, C=100, class_weight=dic_labels)
     #Obrim el fitxer Features.txt de train
     bow_train= open((os.path.join(params['root'],params['database'],'train','Features.txt')),'r')
     #Creem i omplim el diccionari de train
-    ftrain=dict()
-    ftrain=pk.load(bow_train)
-    #Creem una llista amb les ImageIDs del diccionari
-    lftrain=list()
-    lftrain=ftrain.keys()
-    #Creem una llista amb les "features" de les ImageIDs
-    for tkey in lftrain:
-        lftrain.append(ftrain[tkey])
+    dftrain=dict()
+    dftrain=pk.load(bow_train)
     #Reobrim el fitxer de les anotacions
     annotation = open(os.path.join(params['root'],params['database'],'train','annotation.txt'),'r')
     #Saltem la primera linia del fitxer
     annotation.readline()
-    #Per realitzar el pas seguent creem un diccionari amb les ImageIDs de train i el label
+    #Creem un diccionari amb les ImageIDs de train i el label
     dklab=dict()
     for line in annotation:
         #Afegim a la variable label totes les classes tretes del .txt de les annotacions
@@ -50,26 +44,19 @@ def train_classifier(params):
         dklab[line.split()[0]]=line.split()[1]
     annotation.close()
     
-    #Creem una llista de 2 columnes, la primera amb els "labels" i la segona amb les "features"
-    #Ej:  Classe2   Features[0]
-    #     Classe4   Features[1]
-    #     Classe2   Features[2]
-    llf=[]
-    i=0
-    for key in lftrain:
-        #Mirar ejemplo: http://www.dotnetperls.com/2d-list-python
-        llf=llf[i].append(dklab[key])
-        llf=llf[i].append(ftrain[key])
-        if i<len(dklab):
-            llf.append()
-        i=i+1
-    
-    
-    
+    #Creem dues llistes on hi hagin les "features" i els "labels" ORDENATS PER IMATGE!!
+    lf=list()
+    ll=list()
+    for key in dklab.keys():
+        ll.append(dklab[key])
+        lf.append(dftrain[key])
+        
+    lf=np.array(lf)
+    ll=np.array(ll)
     #Entrenem el model SVM amb les dades d'entrenament
-    clf.fit(llf,labels) # Mirar ejemplo de Model Persistence: http://scikit-learn.org/stable/tutorial/basic/tutorial.html
+    clf.fit(lf,ll) # Mirar ejemplo de Model Persistence: http://scikit-learn.org/stable/tutorial/basic/tutorial.html
     
-    #Guardem el model a memòria. El pk.dumps guarda el model en un string en comptes de en un fitxer
-    trained_model=pk.dumps(clf)
-    #Per carregarlo: model=pk.loads(trained_model)
-    return trained_model
+    #Guardem el model a memoria. El pk.dumps guarda el model en un string en comptes de en un fitxer
+    trained_model = open (os.path.join(params['root'],params['database'],'train','Trained_model.txt'), 'w')
+    pk.dump(clf,trained_model)
+    trained_model.close()
