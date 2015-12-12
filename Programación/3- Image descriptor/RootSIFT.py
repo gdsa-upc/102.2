69d701f3aff979b72196b8af85a4a91ec8905f0f
Adrià Zarzuela <adri_zarzu@hotmail.com>
2015-12-12 14:12:13 +0100

    Update RootSIFT.py

--- Programación/3- Image descriptor/RootSIFT.py
+++ Programación/3- Image descriptor/RootSIFT.py
@@ -1,25 +1,22 @@
-# import the necessary packages
 import numpy as np
 import cv2
  
 class RootSIFT:
 	def __init__(self):
-		# initialize the SIFT feature extractor
+		#Inicialització del self.extractor
 		self.extractor = cv2.DescriptorExtractor_create("SIFT")
  
 	def compute(self, image, kps, eps=1e-7):
-		# compute SIFT descriptors
+		#Calcul dels descriptors SIFT
 		(kps, descs) = self.extractor.compute(image, kps)
  
-		# if there are no keypoints or descriptors, return an empty tuple
+		#Sino hi ha kp, retorna un vector buit
 		if len(kps) == 0:
 			return ([], None)
  
-		# apply the Hellinger kernel by first L1-normalizing and taking the
-		# square-root
 		descs /= (descs.sum(axis=1, keepdims=True) + eps)
 		descs = np.sqrt(descs)
 		#descs /= (np.linalg.norm(descs, axis=1, ord=2) + eps)
  
-		# return a tuple of the keypoints and descriptors
+		#Retorn dels keypoints i dels descriptors
 		return (kps, descs)
