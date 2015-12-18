4638eb1512b8826e76393f2605911cf543d2c4d4
Carles Morales <carlesm.95@gmail.com>
2015-12-18 19:35:19 +0100

    Update train_codebook.py

--- Programación/3- Image descriptor/train_codebook.py
+++ Programación/3- Image descriptor/train_codebook.py
@@ -2,6 +2,6 @@ from sklearn.cluster import MiniBatchKMeans
 
 def train_codebook(params,descriptores,paraules):
     km = MiniBatchKMeans(paraules)
-    #Entrenem el KMeans, ajustem el número de clusters a 4
+    #Entrenem el KMeans, ajustem el numero de clusters a 4
     km.fit(descriptores)
     return km
