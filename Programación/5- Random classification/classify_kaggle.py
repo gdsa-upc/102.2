9950d10bf28ed9ac3b9d029d29d8baf0b777bbaa
Ivancaminal72 <ivancaminal72@gmail.com>
2015-12-20 00:20:44 +0100

    Rank i Classify en format kaggle

---
+++ Programación/5- Random classification/classify_kaggle.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+import cPickle as pk
+import numpy as np
+
+def classify(params):
+    #Obrim el model entrenat
+    train_model= open((os.path.join(params['root'],params['database'],'train','Trained_model.txt')),'r')
+    #Obrim el fitxer de Bag of Word de validacio
+    fval= open((os.path.join(params['root'],params['database'],'val','Features.txt')),'r')
+    #Declarem el diccionari
+    dval=dict()
+    #Carreguem la informacio als dictionaries
+    clf2=pk.load(train_model)
+    dval=pk.load(fval)
+    dickeys=[]
+    diclist=[]
+    for key,value in dval.iteritems():
+        #Afegim la key i value
+        dickeys.append(key)
+        diclist.append(value)
+    diclist=np.array(diclist)
+    prediction=clf2.predict(diclist)
+    fclassi = open((os.path.join(params['root'],params['database'],'val','Resultats_classify.txt')),'w')
+    fclassi.write('ImageID'+","+'ClassID'+"\n")
+    #Per cada imatge de Validacio generem una classificacio 
+    i=0
+    for key in dickeys:
+        fclassi.write(key+","+prediction[i]+"\n")
+        i=i+1
+    #Tanquem els fitxers de característiques de validació i train
+    fval.close()
+    fclassi.close()
+    
+    
