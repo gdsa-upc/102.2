f966ecaaae88cbf003846a3022059652d7b7136f
Adrià Zarzuela <adri_zarzu@hotmail.com>
2016-01-11 22:12:12 +0100

    Update correcciofotos.m

--- Programación/rotacion imagenes/correcciofotos.m
+++ Programación/rotacion imagenes/correcciofotos.m
@@ -1,5 +1,7 @@
+%Llegir totes les imatges de test (en aquest cas)
 lee_archivos = dir('C:\Users\Gerard\Documents\Universidad\TercerCurso\GDSA\Proyecto\TerrassaBuildings900\TerrassaBuildings900\test\images\*.jpg');
 
+%Bucle per modificar les imatges
 for k = 1:length(lee_archivos)
     archivo = lee_archivos(k).name;
     nombre='C:\Users\Gerard\Documents\Universidad\TercerCurso\GDSA\Proyecto\TerrassaBuildings900\TerrassaBuildings900\test\images\';   
